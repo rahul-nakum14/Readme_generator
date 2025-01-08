490935797131cb1b438722a98ec836c25080bbe2
rahul-nakum14 <106817606+rahul-nakum14@users.noreply.github.com>
2025-01-08 15:23:11 +0000

    Working Deploy

---
+++ github-readme-generator-ai/env.d.ts
@@ -0,0 +1,13 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+      BACKEND_URL: string;
+      BACKEND_README_GENERATE: string;
+      SMTP_SERVER: string;
+      SMTP_USERNAME: string;
+      SMTP_PASSWORD: string
+      SMTP_PORT: string;
+
+    }
+  }
+  
+  
